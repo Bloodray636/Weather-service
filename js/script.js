294b51e57a1cb3e5a8999971a628f355587863f8
Bloodray636 <d.korolev2003666@bk,ru>
2023-05-07 18:05:59 +0600

    Weather service

---
+++ js/script.js
@@ -0,0 +1,62 @@
+function change(){
+	document.getElementById("degrees1").innerHTML="59°";
+	document.getElementById("degrees2").innerHTML="81°";
+	document.getElementById("degrees3").innerHTML="68°";
+	document.getElementById("degrees4").innerHTML="69°";
+	document.getElementById("degrees5").innerHTML="55°";
+	document.getElementById("degrees6").innerHTML="59°";
+	document.getElementById("degrees7").innerHTML="48°";
+	document.getElementById("degrees8").innerHTML="68°";
+	document.getElementById("degrees9").innerHTML="48°";
+	document.getElementById("degrees10").innerHTML="75°";
+	document.getElementById("degrees11").innerHTML="62°";
+	document.getElementById("degrees12").innerHTML="68°";
+	document.getElementById("degrees13").innerHTML="42°";
+	document.getElementById("degrees14").innerHTML="69°";
+	document.getElementById("degrees15").innerHTML="51°";
+	document.getElementById("degrees16").innerHTML="66°";
+	document.getElementById("degrees17").innerHTML="55°";
+	document.getElementById("degrees18").innerHTML="60°";
+	document.getElementById("degrees19").innerHTML="51°";
+	document.getElementById("degrees20").innerHTML="68°";
+	document.getElementById("degrees21").innerHTML="53°";
+}
+
+function change2(){
+	document.getElementById("degrees1").innerHTML="15°";
+	document.getElementById("degrees2").innerHTML="27°";
+	document.getElementById("degrees3").innerHTML="20°";
+	document.getElementById("degrees4").innerHTML="21°";
+	document.getElementById("degrees5").innerHTML="13°";
+	document.getElementById("degrees6").innerHTML="15°";
+	document.getElementById("degrees7").innerHTML="9°";
+	document.getElementById("degrees8").innerHTML="20°";
+	document.getElementById("degrees9").innerHTML="9°";
+	document.getElementById("degrees10").innerHTML="24°";
+	document.getElementById("degrees11").innerHTML="17°";
+	document.getElementById("degrees12").innerHTML="20°";
+	document.getElementById("degrees13").innerHTML="6°";
+	document.getElementById("degrees14").innerHTML="21°";
+	document.getElementById("degrees15").innerHTML="11°";
+	document.getElementById("degrees16").innerHTML="19°";
+	document.getElementById("degrees17").innerHTML="13°";
+	document.getElementById("degrees18").innerHTML="16°";
+	document.getElementById("degrees19").innerHTML="11°";
+	document.getElementById("degrees20").innerHTML="20°";
+	document.getElementById("degrees21").innerHTML="19°";
+}
+
+function change3(){
+	document.getElementById("degrees22").innerHTML="59°";
+	document.getElementById("degrees23").innerHTML="68°";
+	document.getElementById("degrees24").innerHTML="48°";
+}
+
+function change4(){
+	document.getElementById("degrees22").innerHTML="15°";
+	document.getElementById("degrees23").innerHTML="20°";
+	document.getElementById("degrees24").innerHTML="9°";
+}
+
+        
+       
